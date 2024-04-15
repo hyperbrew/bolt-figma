1f52a2a6e17a5e0ef8726463a3fb7414da7631b6
Justin Taylor <justin@justintaylor.tv>
2024-04-15 12:22:41 -0700

    simplify code project

--- code/src/code.ts
+++ code/src/code.ts
@@ -1,3 +1,5 @@
+import { add } from "./lib";
+
 figma.showUI(__html__, { width: 550, height: 600 });
 
 figma.currentPage.children;
@@ -10,6 +12,7 @@ const getStore = async (key: string) => {
 };
 
 const setStore = async (key: string, value: string) => {
+  const res = add(1, 2);
   console.log("set store ", key, value);
   await figma.clientStorage.setAsync(key, value);
 };
@@ -31,5 +34,9 @@ figma.ui.onmessage = async (msg: any) => {
 
   if (msg.func === "close") {
     figma.closePlugin();
+  } else if (msg.func === "getStore") {
+    getStore(msg.key);
+  } else if (msg.func === "setStore") {
+    setStore(msg.key, msg.value);
   }
 };
--- code/vite.config.ts
+++ code/vite.config.ts
@@ -1,32 +1,37 @@
 import { defineConfig } from "vite";
 import type { Plugin } from "vite";
 import type { OutputChunk } from "rollup";
+import { resolve } from "path";
 
 const figmaCodePlugin: () => Plugin = () => ({
   name: "vite-figma-plugin",
   generateBundle(output, bundle) {
-    // console.log("generateBundle");
-    let code = "";
-    Object.keys(bundle).forEach((file) => {
-      const current = bundle[file] as OutputChunk;
-      console.log("name:", current.fileName);
-      code += current.code;
-    });
-    this.emitFile({
-      type: "asset",
-      fileName: "code.js",
-      source: code,
-    });
+    let files = Object.keys(bundle);
+    if (files.length > 1) {
+      throw this.error(
+        "More than one file found in code.ts project. Check Vite Config settings"
+      );
+    }
+    bundle[files[0]].fileName = "code.js";
+    return;
   },
-  writeBundle() {
-    console.log("writeBundle");
+  writeBundle(res) {
+    console.log("writeBundle", res);
+    console.log("res.assetFileNames", res.assetFileNames);
   },
 });
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [figmaCodePlugin()],
+  // plugins: [figmaCodePlugin()],
   build: {
     outDir: "dist",
+    rollupOptions: {
+      output: {
+        manualChunks: {},
+        entryFileNames: `code.js`,
+      },
+      input: "/src/code.ts",
+    },
   },
 });
